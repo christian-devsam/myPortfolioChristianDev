ee37f10404f115d1f963cc78196f8dad775f34af
christian-devsam <80765872+christian-devsam@users.noreply.github.com>
2026-01-23 14:10:18 +0000

    Deploying to gh-pages from @ christian-devsam/myPortfolioChristianDev@9f0c9a6c93352715f24bd9a6e489c5fd6e25a6a1 🚀

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,8 +117,8 @@ if "total_tokens" not in st.session_state: st.session_state.total_tokens = 0
 if "last_latency" not in st.session_state: st.session_state.last_latency = 0.0
 if "last_audio_hash" not in st.session_state: st.session_state.last_audio_hash = None
 
-st.title("⚡ Asistente IA de Christian Silva")
-st.markdown("Potenciado por **Groq (Llama 3.3 + Whisper)** + **Embeddings Locales**.")
+st.title("⚡ IA de ChristianS")
+st.markdown("Potenciado por **Groq (Llama 3.3 + Whisper)** + **Embeddings Locales** para leer CV de Christian.")
 
 # --- API KEY & CLIENTE GROQ ---
 try:
@@ -222,11 +222,11 @@ if "process_complete" in st.session_state:
         for message in st.session_state.messages:
             with st.chat_message(message["role"]): st.write(message["content"])
 
-        text_input = st.chat_input("Escribe tu pregunta aquí...", max_chars=1000)
+        text_input = st.chat_input("Ej: ¿Christian sabe SQL?...", max_chars=1000)
 
-        # --- CORRECCIÓN AQUÍ: Inicializamos variables ---
+        
         final_prompt = None
-        transcription = None # <--- ESTO ARREGLA EL ERROR. Siempre existe, aunque sea None.
+        transcription = None 
         
         # 1. Prioridad: Audio
         if audio:
@@ -326,6 +326,7 @@ if "process_complete" in st.session_state:
         )
         
         agraph(nodes=nodes, edges=edges, config=config)
+
 
 
 
